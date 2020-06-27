6ca815e9295907b8b56fa05f1d8be53cd020b6d3
Gustavo Leste <gustavoleste@protonmail.com>
2020-06-27 20:02:07 -0300

    add dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.ts',
@@ -18,4 +19,12 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    devServer: {
+        contentBase: './src',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+        title: 'S.O.L.I.D with TypeScript',
+        }),
+    ],
 };
